5d7e9c5287da321f8ad5ede4a1282d76188bffa2
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-04-18 19:49:39 +0700

    learn pop for list

--- list.py
+++ list.py
@@ -1,13 +1,20 @@
-keranjang = [1, 9.8, " Apel"]
-#len(keranjang)
+# keranjang = [1, 9.8, " Apel"]
+# #len(keranjang)
 
-# for i in range (len(keranjang)) :
-    # print(keranjang[0])
-keranjang[0]="nanas"
+# # for i in range (len(keranjang)) :
+#     # print(keranjang[0])
+# keranjang[0]="nanas"
 
-buahLain = ['jeruk', 'lemon', 'strawbery', 'mangga']
-keranjang = keranjang + buahLain
-len(keranjang)
+# buahLain = ['jeruk', 'lemon', 'strawbery', 'mangga']
+# keranjang = keranjang + buahLain
+# len(keranjang)
 
-for x in range (len(keranjang)):
-    print(keranjang)
+# for x in range (len(keranjang)):
+#     print(keranjang)
+
+# ============================================================
+# Memotong isi sebuah list
+
+makanan = ["ayam", "soto", "sop", "sate"]
+makanan.pop(2) # .POP digunakan untuk memotong sebuah List.
+print(makanan)
