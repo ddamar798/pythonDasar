8a349930c97be1376ee6fe63a0f2f7d9efa37446
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-04-18 18:54:54 +0700

    learn list data

---
+++ list.py
@@ -0,0 +1,13 @@
+keranjang = [1, 9.8, " Apel"]
+#len(keranjang)
+
+# for i in range (len(keranjang)) :
+    # print(keranjang[0])
+keranjang[0]="nanas"
+
+buahLain = ['jeruk', 'lemon', 'strawbery', 'mangga']
+keranjang = keranjang + buahLain
+len(keranjang)
+
+for x in range (len(keranjang)):
+    print(keranjang)
