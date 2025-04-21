411ade4c04af7262e810d9cbf24aaf1b2115f666
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-04-21 18:44:39 +0700

    keterangan

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+# PERBEDAAN DICTIONARY DENGAN LIST
+              
+# Dictionary
+    # - Tidak memiliki urutan.
+    # - Pengambilan item denggan cara menggunakan 'key'.
+    # - Format -> dict1 = {'key1':item, 'key2':item, 'key3':item}
+
+# List
+    # - Memiliki urutan.
+    # - Pemanggilan item dengan memanggil index-nya (urutan item).
+    # - Format -> list1 = [item1,item2,item3].
