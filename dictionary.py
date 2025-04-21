c0934d9461c942c0e72e9b02fab0f8c0cd1f3b29
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-04-21 18:46:36 +0700

    add readme

--- dictionary.py
+++ dictionary.py
@@ -9,3 +9,8 @@
     # - Memiliki urutan.
     # - Pemanggilan item dengan memanggil index-nya (urutan item).
     # - Format -> list1 = [item1,item2,item3].
+
+#==================================================================
+    # Praktek
+
+    
