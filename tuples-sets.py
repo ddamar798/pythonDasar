ff602c4b94d9dfed300ce20e16e79c3e8e784695
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-04-24 18:48:16 +0700

    mengubah isi tuple

--- tuples-sets.py
+++ tuples-sets.py
@@ -1,3 +1,7 @@
 keranjangT = (1,2,3,4,5,6,'a','c')  
 cekTipe = type(keranjangT)
-print (cekTipe)
+
+# Mengubah isi pada Tuple
+t1 = (1,2,3)
+t1 = t1[0],'rorr',t1[2] # Cara yng tdk efisien, Lebih baik menggunakan looping.
+print (t1)
