277274ffdb95148d57d76cc81a7b6aa36481e410
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-04-26 19:49:06 +0700

    fix

--- tuples-sets.py
+++ tuples-sets.py
@@ -6,7 +6,7 @@ t1 = (1,2,3)
 t1 = t1[0],'rorr',t1[2] # Cara yng tdk efisien, Lebih baik menggunakan looping.
 print (t1)
 
-# Methods di tuples
+# Methods di tuples    
 x1 = (9,9,9,3,3,3,3,3)
 print( x1.count(9) ) # <- Fungsi x1.count adalah untuk menghitung jumlah isi di dalam tuples tersebut.
 
