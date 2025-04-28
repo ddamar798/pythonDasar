2aad52d681407d559177a2bf556125441ef51432
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-04-28 18:47:43 +0700

    comparison operator

--- boolean.py
+++ boolean.py
@@ -1,3 +1,13 @@
 #Boolean: Tipe data di python.
 #cek = type(False/True) - True/False wajib diawali denggan huruf besar.
 #print(cek)
+
+# COMPARISON OPERATORS
+
+#   Operator    |   Keterangan
+#     ==        |   sama dengan
+#     !=        |   tidak sama dengan
+#     >         |   lebih besar dari (eklusif)
+#     <         |   lebih kecil dari (eklusif)
+#     >=        |   lebih besar dari (inklusif)
+#     <=        |   lebih kecil dari (inklusif)
