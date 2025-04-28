b7e374978873a1d06385a0dc3154fcaefc83d75f
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-04-28 18:26:51 +0700

    clear

--- tuples-sets.py
+++ tuples-sets.py
@@ -4,11 +4,11 @@ cekTipe = type(keranjangT)
 # Mengubah isi pada Tuple
 t1 = (1,2,3)
 t1 = t1[0],'rorr',t1[2] # Cara yng tdk efisien, Lebih baik menggunakan looping.
-print (t1)
+#print (t1)
 
 # Methods di tuples    
 x1 = (9,9,9,3,3,3,3,3)
-print( x1.count(9) ) # <- Fungsi x1.count adalah untuk menghitung jumlah isi di dalam tuples tersebut.
+#print( x1.count(9) ) # <- Fungsi x1.count adalah untuk menghitung jumlah isi di dalam tuples tersebut.
 
 # Sets
 # Sets adalah sebuah tipe data untuk elemen yang bersifat unik (tanpa duplikasi) dan tidak beraturan.
