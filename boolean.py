1c9a44a5eeeaf7ba497fafdb8092b0b2c23e67aa
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-01 18:26:10 +0700

    boolean

--- boolean.py
+++ boolean.py
@@ -20,4 +20,5 @@
 # COMPARISON OPERATORS BERANTAI
 #===============================
 angka1 = 3<4 and 4>3 #menghasilkan 'True'. "and" harus kedua Value benar untuk menghasilkan True.
-print(angka1)
+angka2 = 4<5 or 4>2  #menhasilkan 'True'. "or" cukup salah satu Value benar untuk menghasilkan True.
+print(angka2)
