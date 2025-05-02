9fd9ab72dbdaec3336ab739e70777363ad3c78ed
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-02 20:36:00 +0700

    fix

--- boolean.py
+++ boolean.py
@@ -22,3 +22,5 @@
 angka1 = 3<4 and 4>3 #menghasilkan 'True'. "and" harus kedua Value benar untuk menghasilkan True.
 angka2 = 4<5 or 4>2  #menhasilkan 'True'. "or" cukup salah satu Value benar untuk menghasilkan True.
 print(angka2)
+
+   
