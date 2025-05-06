9c67ece11d43816163e27a5b063826443cd5afa6
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-06 19:15:47 +0700

    fix

--- looping.py
+++ looping.py
@@ -27,7 +27,7 @@
 angka = [1,2,3,4,5,6,7,8,9,10]
 
 for item in angka:
-    if item%2 == 0:
+    if item%2 == 0:   
         print('Angka {} adalah angka Genap!', format(item))
     else :
         print ('Angka {} adalah angka Ganjil!', format(item))
