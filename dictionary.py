1390e743bb5e1434ebf9c457b31aaf714a5c7a4b
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-13 20:07:59 +0700

    add function file

--- dictionary.py
+++ dictionary.py
@@ -9,7 +9,7 @@
     # - Memiliki urutan.
     # - Pemanggilan item dengan memanggil index-nya (urutan item).
     # - Format -> list1 = [item1,item2,item3].     
-
+      
 #==================================================================
     # Praktek
 
