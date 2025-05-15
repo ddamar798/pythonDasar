aee37ac2edcd6ca8ba3b23e218a8065809a1dc2e
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-15 20:14:15 +0700

    learn Args & Kwargs

--- argsKwargs.py
+++ argsKwargs.py
@@ -1,3 +1,11 @@
 # Args dan Kwargs
 
 #Args : Argumen dimana kita bisa memasukan sekian banyak Variable.
+'''
+def cekArgs(*args):
+    return sum((args))
+
+print(cekArgs(2,3)) # Menghasilkan 5
+'''
+
+# Kwargs : memasukan sekian banyak item Dictionary
