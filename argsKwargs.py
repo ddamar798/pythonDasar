940c9152adc979578ed2955a116cdea71507e637
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-15 20:21:04 +0700

    learn Args & Kwargs

--- argsKwargs.py
+++ argsKwargs.py
@@ -9,3 +9,10 @@ print(cekArgs(2,3)) # Menghasilkan 5
 '''
 
 # Kwargs : memasukan sekian banyak item Dictionary
+def cekBerat(**kwargs):
+    if 'Tono' in kwargs:
+        print('Berat Tono Tergolong ke kategori {}'. format(kwargs['Tono']))
+    else:
+        print('Berat Tono tidak masuk dalam database kita!')
+
+print(cekBerat(Tono='gemuk'))
